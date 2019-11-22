f348769d238b2a325ad774339a0b3fa5487d2149
Oleksiy Shapovalov <oleksiy.shapovalov@gmail.com>
2019-11-22 11:51:04 +0200

    Added possibility for local debugging, small refactoring

--- popup.js
+++ popup.js
@@ -1,48 +1,28 @@
 // Inject the payload.js script into the current tab after the popout has loaded
 window.addEventListener('load', function (evt) {
-    chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
-        file: 'payload.js'
-    }, () => chrome.runtime.lastError);;
-    
+    if (chrome && chrome.extension) {
+        chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+            file: 'payload.js'
+        }, () => chrome.runtime.lastError);;
+    } else {
+        init();
+    }
 });
 
-// Listen to messages from the payload.js script and write to popout.html
-chrome.runtime.onMessage.addListener(function (message) {
-    function drawChanges(elementId, newRate) {
-        var storage = window.localStorage;
-        var element = document.getElementById(elementId);
-        var currentKey = elementId + "Current";
-        var previousKey = elementId + "Previous";
-        var currentRate = storage.getItem(currentKey);
-        var previousRate = storage.getItem(previousKey);
-        if (currentRate === null || previousRate === null){
-            currentRate = previousRate = newRate;
-            storage.setItem(currentKey, newRate);
-            storage.setItem(previousKey, newRate);
-        }
-        var diff = (newRate - currentRate);
-        if (diff > 0.0001 || diff < -0.0001) {
-            previousRate = currentRate;
-            currentRate = newRate;
-            storage.setItem(currentKey, currentRate);
-            storage.setItem(previousKey, previousRate);
-        }
-        var text = (newRate - previousRate).toFixed(3);
-        element.innerHTML = text;
-        if (currentRate < previousRate) {
-            element.style.backgroundImage = "url('down.png')";
-        } else if (currentRate > previousRate) {
-            element.style.backgroundImage = "url('up.png')";
-            element.innerHTML = "+" + element.innerHTML;
-        } else {
-            element.style.background = "none"
-            element.innerHTML = "0.000";
-        }
-        element.style.backgroundRepeat = "no-repeat";
-        element.style.backgroundPositionX = "6px";
-        element.style.backgroundPositionY = "4px";
-    }
+var isPrivatEnabled = true;
+var isMonoEnabled = true;
+var idKurscomuaEnabled = true;
 
+function init (message) {
+    loadPrivat();
+    loadMono();
+    loadKurscomua();
+}
+
+function loadPrivat () {
+    if (!isPrivatEnabled) {
+        return;
+    }
     fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11')
     .then(response => response.json())
     .then(data => {
@@ -59,7 +39,12 @@ chrome.runtime.onMessage.addListener(function (message) {
             document.getElementById('privatbank').innerHTML = (+usd).toFixed(3);
         }
     });
+}
 
+function loadMono () {
+    if (!isMonoEnabled) {
+        return;
+    }
     fetch('https://api.monobank.ua/bank/currency')
     .then(response => response.json())
     .then(data => {
@@ -76,24 +61,13 @@ chrome.runtime.onMessage.addListener(function (message) {
             document.getElementById('monobank').innerHTML = (+monousd).toFixed(3);
         }
     });
+}
 
-    function getLatestRate(data) {
-        console.log(data);
-        if (data.length > 0) {
-            var i = data.length - 1;
-            do {
-                if (data[i][1] !== null) {
-                    return data[i][1];
-                }
-            } while (--i >= 0)
-        }
-        return null;
+function loadKurscomua () {
+    if (!idKurscomuaEnabled) {
+        return;
     }
-
     fetch('https://kurs.com.ua/ajax/getChart?size=big&type=interbank&currencies_from=usd&currencies_to=&organizations=&limit=&optimal=', {
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest'
-        }
     })
     .then(response => response.json())
     .then(data => {
@@ -115,4 +89,57 @@ chrome.runtime.onMessage.addListener(function (message) {
             document.getElementById('kurscomua').innerHTML = (+kursRate).toFixed(3);
         }
     });
-});
+}
+
+function drawChanges(elementId, newRate) {
+    var storage = window.localStorage;
+    var element = document.getElementById(elementId);
+    var currentKey = elementId + "Current";
+    var previousKey = elementId + "Previous";
+    var currentRate = storage.getItem(currentKey);
+    var previousRate = storage.getItem(previousKey);
+    if (currentRate === null || previousRate === null){
+        currentRate = previousRate = newRate;
+        storage.setItem(currentKey, newRate);
+        storage.setItem(previousKey, newRate);
+    }
+    var diff = (newRate - currentRate);
+    if (diff > 0.0001 || diff < -0.0001) {
+        previousRate = currentRate;
+        currentRate = newRate;
+        storage.setItem(currentKey, currentRate);
+        storage.setItem(previousKey, previousRate);
+    }
+    var text = (newRate - previousRate).toFixed(3);
+    element.innerHTML = text;
+    if (currentRate < previousRate) {
+        element.style.backgroundImage = "url('down.png')";
+    } else if (currentRate > previousRate) {
+        element.style.backgroundImage = "url('up.png')";
+        element.innerHTML = "+" + element.innerHTML;
+    } else {
+        element.style.background = "none"
+        element.innerHTML = "0.000";
+    }
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPositionX = "6px";
+    element.style.backgroundPositionY = "4px";
+}
+
+function getLatestRate(data) {
+    console.log(data);
+    if (data.length > 0) {
+        var i = data.length - 1;
+        do {
+            if (data[i][1] !== null) {
+                return data[i][1];
+            }
+        } while (--i >= 0)
+    }
+    return null;
+}
+
+if (chrome && chrome.runtime && chrome.runtime.onMessage) {
+    // Listen to messages from the payload.js script and write to popout.html
+    chrome.runtime.onMessage.addListener(init);
+}
